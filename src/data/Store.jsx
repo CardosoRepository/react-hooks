142fe58f661a81d092586a9e46e643c19b6fcafb
Cardoso <gabrielcard0s0@hotmail.com>
2023-09-07 18:34:13 -0300

    Aula 75: useContext mais regrado

--- src/data/Store.jsx
+++ src/data/Store.jsx
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+
+const initialState = {
+    number: 1234,
+    text: 'Context API + Hooks'
+};
+
+export const AppContext = React.createContext(initialState);
 
 const Store = (props) => {
+    const [state, setState] = useState(initialState);
+
+    function updateState(key, value) {
+        setState({
+            ...state,
+            [key]: value
+        })
+    }
+
     return (
-
-        <div className="Store">
-            Store
-        </div>
-
+        <AppContext.Provider value={{
+            number: state.number,
+            text: state.text,
+            setNumber: n => updateState('number', n),
+            setText: t => updateState('text', t),
+        }}>
+            <div className="Store">
+                {props.children}
+            </div>
+        </AppContext.Provider>
     );
 };
 
--- src/views/examples/UseContext.jsx
+++ src/views/examples/UseContext.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import PageTitle from '../../components/layout/PageTitle'
 import DataContext from '../../data/DataContext'
 import { useContext } from 'react'
+import SectionTitle from '../../components/layout/SectionTitle';
+import { AppContext } from '../../data/Store';
 
 const UseContext = (props) => {
     const {state, setState} = useContext(DataContext);
+    const {number, text, setNumber} = useContext(AppContext);
 
     function addNumber(delta) {
         setState({
@@ -12,12 +15,20 @@ const UseContext = (props) => {
             number: state.number + delta
         })
     }
+
+    useEffect(function() {
+        if (number > 1250) {
+            console.log('Eitaa!!');
+        };
+    }, [number])
     return (
         <div className="UseContext">
             <PageTitle
                 title="Hook UseContext"
                 subtitle="Aceita um objeto de contexto e retorna o valor atual do contexto!"
             />
+
+            <SectionTitle title="Exercício #01"/>
             <div className="center">
                 <span className="text">{state.text}</span>
                 <span className="text">{state.number}</span>
@@ -25,6 +36,18 @@ const UseContext = (props) => {
                 <div>
                     <button className="btn" onClick={() => addNumber(-1)}>-1</button>
                     <button className="btn" onClick={() => addNumber(1)}>+1</button>
+                </div>
+            </div>
+
+            <SectionTitle title="Exercício #02" />
+            <div className="center">
+                <span className="text">{text}</span>
+                <span className="text">{number}</span>
+                <div>
+                    <button className="btn"
+                        onClick={() => setNumber(number - 1)}>-1</button>
+                    <button className="btn"
+                        onClick={() => setNumber(number + 1)}>+1</button>
                 </div>
             </div>
         </div>
